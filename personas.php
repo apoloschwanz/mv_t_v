ba554793be82d6b7375b28bc4156b38a0b1d64ce
Roquet <daniel.zabalet@gmail.com>
2016-11-24 17:14:26 -0300

    agregado personas - comienzo

---
+++ personas.php
@@ -0,0 +1,20 @@
+<?php
+	//
+	// Control Sesion
+	require_once 'controlsesion.php' ;
+	//
+	// Inclusion de clases 
+	require_once 'class_paginai.php' ;
+	//
+	// Clase de la entidad
+	require_once 'clase_personas.php' ;
+	//
+	// Instancia Entidad
+	$pagina = new personas() ;
+	
+	//
+	// Mustra Pagina Web de la entidad
+	$pagina->mostrar_pagina_lista();
+	
+	
+?>
